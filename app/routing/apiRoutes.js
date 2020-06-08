1287eb32c9c90f7783db4cdc22d680195ae9e135
Andrea Sanchez <andrea.sancheztx@gmail.com>
2020-06-07 21:48:47 -0500

    updates

--- app/routing/apiRoutes.js
+++
@@ -1,9 +0,0 @@
-var friends = require("../data/friends")
-
-module.exports = function (app) {
-
-    app.get("/api/friends", function (req, res) {
-        console.log(friends)
-        res.json(friends)
-    })
-}
